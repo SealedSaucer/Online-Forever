936e4ed666692e7e7f5fa99a1ba8b2357411f2e1
Tenshi <57001729+Tenshi147@users.noreply.github.com>
2022-01-30 13:40:26 +0000

    main.py Fix status not changing
    The status would not change for my account, added an event to fix this and also added the other status types of which you could give the option to config at some point

--- main.py
+++ main.py
@@ -5,8 +5,21 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix=':', self_bot=True, help_command=None)
 
+
+@client.event
 async def on_ready():
-  await client.change_presence(status=discord.Status.online, activity=discord.Game("TESTING"))
+    # Playing
+    # await client.change_presence(activity=discord.Game(name="a game"))
+
+    # Streaming
+    # await bot.change_presence(activity=discord.Streaming(name="a Stream", url="http://your-url-here.com/"))
+
+    # Listening
+    # await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="a song"))
+
+    # Watching
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="The world burn"))
+
 
 keep_alive.keep_alive()
 client.run(os.getenv("TOKEN"), bot=False)
