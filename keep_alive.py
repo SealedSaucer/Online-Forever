11280bb2e4c63098e5842c71de75bf811de4f0f9
Phantom <71928730+SealedSaucer@users.noreply.github.com>
2022-03-23 19:13:30 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def main():
-    return '<meta http-equiv="refresh" content="0; URL=https://phantom.codes/support"/>'
+    return '<meta http-equiv="refresh" content="0; URL=https://phantom.is-a.dev/support"/>'
 
 def run():
     app.run(host="0.0.0.0", port=8080)
