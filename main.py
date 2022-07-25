7118698b86187b8bae57a7f7e3c3b340fa72f523
Phantom <71928730+SealedSaucer@users.noreply.github.com>
2022-07-25 15:30:20 +0530

    Pushed some changes from @rhyd0n and @Tenshi147

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import discord
 import os
-import keep_alive
+from keep_alive import keep_alive
 from discord.ext import commands
 
 client = commands.Bot(command_prefix=':', self_bot=True, help_command=None)
 
+@client.event
 async def on_ready():
-  await client.change_presence(status=discord.Status.online, activity=discord.Game("TESTING"))
+  await client.change_presence(status=discord.Status.online)
 
-keep_alive.keep_alive()
+keep_alive()
 client.run(os.getenv("TOKEN"), bot=False)
