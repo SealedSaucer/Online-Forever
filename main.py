046449bbeea273bb3197cdedecb51b83e79b686f
Phantom <71928730+SealedSaucer@users.noreply.github.com>
2022-07-25 15:38:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ client = commands.Bot(command_prefix=':', self_bot=True, help_command=None)
 @client.event
 async def on_ready():
   await client.change_presence(status=discord.Status.online)
+  os.system('clear')
+  print(f'Logged in as {client.user} (ID: {client.user.id})')
 
 keep_alive()
 client.run(os.getenv("TOKEN"), bot=False)
