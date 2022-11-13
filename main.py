04fbab1b00642a51dd5e316b479c8105f900727d
Phantom <71928730+SealedSaucer@users.noreply.github.com>
2022-11-13 18:06:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
-import discord
+import json
+import time
+import websocket
+import requests
 import os
 from keep_alive import keep_alive
-from discord.ext import commands
 
-client = commands.Bot(command_prefix=':', self_bot=True, help_command=None)
+status = "online"
 
-@client.event
-async def on_ready():
-  await client.change_presence(status=discord.Status.online)
-  os.system('clear')
-  print(f'Logged in as {client.user} (ID: {client.user.id})')
+headers = {"Authorization": os.getenv("TOKEN"), "Content-Type": "application/json"}
+userinfo = requests.get('https://discordapp.com/api/v9/users/@me', headers=headers).json()
+username = userinfo["username"]
+discriminator = userinfo["discriminator"]
+userid = userinfo["id"]
+
+def onliner(token, status):
+    ws = websocket.WebSocket()
+    ws.connect('wss://gateway.discord.gg/?v=9&encoding=json')
+    start = json.loads(ws.recv())
+    heartbeat = start['d']['heartbeat_interval']
+    auth = {"op": 2,"d": {"token": token,"properties": {"$os": "Windows 10","$browser": "Google Chrome","$device": "Windows"},"presence": {"status": status,"afk": False}},"s": None,"t": None}
+    ws.send(json.dumps(auth))
+    online = {"op":1,"d":"None"}
+    time.sleep(heartbeat / 1000)
+    ws.send(json.dumps(online))
+
+def run_onliner():
+  os.system("clear")
+  print(f"Logged in as {username}#{discriminator} ({userid}).")
+  while True:
+    onliner(os.getenv("TOKEN"), status)
+    time.sleep(30)
 
 keep_alive()
-client.run(os.getenv("TOKEN"))
+run_onliner()
