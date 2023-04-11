2410d4b24a879343babc5bf125697df955999524
Phantom <71928730+SealedSaucer@users.noreply.github.com>
2023-04-11 14:06:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,42 +6,80 @@ import requests
 import websocket
 from keep_alive import keep_alive
 
-status = "online"
+status = "dnd"
+
+custom_status = "youtube.com/@SealedSaucer"
 
 usertoken = os.getenv("TOKEN")
 if not usertoken:
-  print("[ERROR] Please add a token inside Secrets.")
-  sys.exit()
+    print("[ERROR] Please add a token inside Secrets.")
+    sys.exit()
 
 headers = {"Authorization": usertoken, "Content-Type": "application/json"}
 
-validate = requests.get('https://discordapp.com/api/v9/users/@me', headers=headers)
+validate = requests.get("https://discordapp.com/api/v9/users/@me", headers=headers)
 if validate.status_code != 200:
-  print("[ERROR] Your token might be invalid. Please check it again.")
-  sys.exit()
+    print("[ERROR] Your token might be invalid. Please check it again.")
+    sys.exit()
 
-userinfo = requests.get('https://discordapp.com/api/v9/users/@me', headers=headers).json()
+userinfo = requests.get("https://discordapp.com/api/v9/users/@me", headers=headers).json()
 username = userinfo["username"]
 discriminator = userinfo["discriminator"]
 userid = userinfo["id"]
 
 def onliner(token, status):
     ws = websocket.WebSocket()
-    ws.connect('wss://gateway.discord.gg/?v=9&encoding=json')
+    ws.connect("wss://gateway.discord.gg/?v=9&encoding=json")
     start = json.loads(ws.recv())
-    heartbeat = start['d']['heartbeat_interval']
-    auth = {"op": 2,"d": {"token": token,"properties": {"$os": "Windows 10","$browser": "Google Chrome","$device": "Windows"},"presence": {"status": status,"afk": False}},"s": None,"t": None}
+    heartbeat = start["d"]["heartbeat_interval"]
+    auth = {
+        "op": 2,
+        "d": {
+            "token": token,
+            "properties": {
+                "$os": "Windows 10",
+                "$browser": "Google Chrome",
+                "$device": "Windows",
+            },
+            "presence": {"status": status, "afk": False},
+        },
+        "s": None,
+        "t": None,
+    }
     ws.send(json.dumps(auth))
-    online = {"op":1,"d":"None"}
+    cstatus = {
+        "op": 3,
+        "d": {
+            "since": 0,
+            "activities": [
+                {
+                    "type": 4,
+                    "state": custom_status,
+                    "name": "Custom Status",
+                    "id": "custom",
+                    #Uncomment the below lines if you want an emoji in the status
+                    #"emoji": {
+                        #"name": "emoji name",
+                        #"id": "emoji id",
+                        #"animated": False,
+                    #},
+                }
+            ],
+            "status": status,
+            "afk": False,
+        },
+    }
+    ws.send(json.dumps(cstatus))
+    online = {"op": 1, "d": "None"}
     time.sleep(heartbeat / 1000)
     ws.send(json.dumps(online))
 
 def run_onliner():
-  os.system("clear")
-  print(f"Logged in as {username}#{discriminator} ({userid}).")
-  while True:
-    onliner(usertoken, status)
-    time.sleep(30)
+    os.system("clear")
+    print(f"Logged in as {username}#{discriminator} ({userid}).")
+    while True:
+        onliner(usertoken, status)
+        time.sleep(30)
 
 keep_alive()
 run_onliner()
