54975599ad2d27fd10638b311289caea0601b5ec
$paket <114870185+packet-syntax@users.noreply.github.com>
2026-07-26 18:11:03 -0600

    Added comments.

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
+# ==============================
+#            Imports
+# ==============================
 import asyncio
 import json
 import requests
 import websockets
 
-TOKEN = "Add your token here"
-STATUS = "online"  # online / dnd / idle
-CUSTOM_STATUS = "Hey!"  # Leave empty if you don't want a custom status
-USE_EMOJI = False
+# ==============================
+#            Configs
+# ==============================
+
+# Key:
+#    * = Default
+#    ! = Required
+
+TOKEN = "Add your token here"        # Your discord token. (! string)
+STATUS = "online"                    # Set status type. (! "online", "dnd", "idle") (* "online")
+CUSTOM_STATUS = "Hey!"               # Custom Status? To dissable leave blank. (* "Hey!")
+USE_EMOJI = False                    # Use a emoji? (! True or False) (* False)
+
+# ==============================
+#            Headers
+# ==============================
 
 headers = {"Authorization": TOKEN}
+
+# ==============================
+#        Requests & Setup
+# ==============================
 
 r = requests.get("https://discord.com/api/v10/users/@me", headers=headers)
 if r.status_code != 200:
@@ -31,6 +50,11 @@ if USE_EMOJI:
         "id": None,     # Required only for custom emojis
         "animated": False
     }
+
+
+# ==============================
+#           Functions
+# ==============================
 
 async def discord_gateway():
     uri = "wss://gateway.discord.gg/?v=10&encoding=json"
